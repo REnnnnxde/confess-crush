cc9b04c616b863461d95d8706fa1fcbbd93ac75e
Renn <119943753+REnnnnxde@users.noreply.github.com>
2025-01-09 23:49:55 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "Pesan Cinta untuk Alia",
+    introTitle: 'Alia Sayangku',
+    introDesc: `Dunia ini memang penuh misteri
+    Tapi kenapa kamu selalu hadir di hati
+    Tersesat di dalam senyumku
+    Di balik cinta ini, ada rasa rindu`,
+    btnIntro: '^^Klik Yuk^^',
+    title: 'Apakah kamu jatuh cinta sejak pandangan pertama 🥰',
+    desc: 'Atau mungkin kamu terpikat sejak melihat senyumku ',
+    btnYes: 'Iyaaa, suka banget <33',
+    btnNo: 'Enggak kok :3',
+    question: 'Di dunia ini ada lebih dari 7 miliar orang, kenapa kamu memilih untuk mencintaiku? <3',
+    btnReply: 'Kirim ke kamu <3',
+    reply: 'Cinta ya cinta, nggak cinta pun tetap cinta <33333333',
+    mess: 'Aku tahu kok 🥰. Aku juga sayang banget sama kamu 😘😘',
+    messDesc: 'bububu',
+    btnAccept: 'Oke, aku tunggu <3',
+    messLink: 'https://wa.me/6282190466407' // Link WhatsApp
 }
